4147dd7889c83d9ad14c9728f7475496c7bb9bd0
Shawn Holcomb <sholcmb@gmail.com>
2022-05-20 22:20:20 -0500

    added google form

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ $(document).ready(function () {
     var whyArr = ["No one else can go so high, high, high, high", "He shows up at the job site, Find a spot that’s just right ", "Outriggers are out", "Reach into the sky", "Strong like you can’t believe, Tons of load capacity", "His cable’s in position, Lifting with precision"];
     var randomWhy = whyArr[Math.floor(Math.random() * whyArr.length)];
     $(".why").text(randomWhy);
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+
+    // var iframe = document.getElementById("google-form");
+    // iframe.onload = function(){
+    //     iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+    // }
 })
 
 // Scroll to top
